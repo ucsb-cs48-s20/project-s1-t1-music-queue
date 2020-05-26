77a30e9e4c8595eb96f66e7c33068a1b1ee6d924
oulily <lilyou@ucsb.edu>
2020-05-25 19:31:09 -0700

    lo - init storybook

--- .next/static/chunks/styles.js
+++ .next/static/chunks/styles.js
@@ -29,7 +29,7 @@
           .querySelectorAll("link[href][rel=stylesheet]")
           .forEach(function(link) {
             if (!link.href.match(prefix) || link.stale) return;
-            injectCss(link, link.href.split("?")[0] + "?unix=1589854138788");
+            injectCss(link, link.href.split("?")[0] + "?unix=1589933031211");
           });
       }
     }
@@ -66,7 +66,7 @@
           .querySelectorAll("link[href][rel=stylesheet]")
           .forEach(function(link) {
             if (!link.href.match(prefix) || link.stale) return;
-            injectCss(link, link.href.split("?")[0] + "?unix=1589854149332");
+            injectCss(link, link.href.split("?")[0] + "?unix=1589933042486");
           });
       }
     }
