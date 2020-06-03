7779491fbfb0198e111e814fa01b2fbbf9d4b574
Gautam Mundewadi <gautammundewadi@gmail.com>
2020-06-03 12:34:47 -0700

    gm-save user votes

--- .next/static/chunks/styles.js
+++ .next/static/chunks/styles.js
@@ -29,7 +29,7 @@
           .querySelectorAll("link[href][rel=stylesheet]")
           .forEach(function(link) {
             if (!link.href.match(prefix) || link.stale) return;
-            injectCss(link, link.href.split("?")[0] + "?unix=1591140333970");
+            injectCss(link, link.href.split("?")[0] + "?unix=1591212851088");
           });
       }
     }
@@ -66,7 +66,7 @@
           .querySelectorAll("link[href][rel=stylesheet]")
           .forEach(function(link) {
             if (!link.href.match(prefix) || link.stale) return;
-            injectCss(link, link.href.split("?")[0] + "?unix=1591140398795");
+            injectCss(link, link.href.split("?")[0] + "?unix=1591212857233");
           });
       }
     }
--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "5ee7f2ef48bc55f2f078";
+/******/ 	var hotCurrentHash = "26a5549e4b2375319074";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
