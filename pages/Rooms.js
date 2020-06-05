776fb05ecea55a346043952a5c36291ae1f883e8
Gautam Mundewadi <gautammundewadi@gmail.com>
2020-06-05 11:19:35 -0700

    gm-fixed merge conflicts

--- pages/Rooms.js
+++ pages/Rooms.js
@@ -16,13 +16,7 @@ class Rooms extends React.Component {
   }
 
   componentDidMount = () => {
-<<<<<<< HEAD
-    let url = window.location.href;
-    if (url.indexOf("_token") > -1) {
-      let access_token = url.split("_token=")[1].split("&")[0].trim();
-=======
     let access_token = getAccessToken()
->>>>>>> master
       this.setState({ access_token });
   };
 
