6cdbfea960df76bbfa663e2daaa71f69a0d3b498
David Farias-Llerenas <dfariasllerenas@ucsb.edu>
2020-06-09 23:34:13 -0700

    jldf - centered loading icon

--- .next/static/chunks/styles.js
+++ .next/static/chunks/styles.js
@@ -29,7 +29,7 @@
           .querySelectorAll("link[href][rel=stylesheet]")
           .forEach(function(link) {
             if (!link.href.match(prefix) || link.stale) return;
-            injectCss(link, link.href.split("?")[0] + "?unix=1591745637074");
+            injectCss(link, link.href.split("?")[0] + "?unix=1591770796596");
           });
       }
     }
@@ -66,7 +66,7 @@
           .querySelectorAll("link[href][rel=stylesheet]")
           .forEach(function(link) {
             if (!link.href.match(prefix) || link.stale) return;
-            injectCss(link, link.href.split("?")[0] + "?unix=1591745448084");
+            injectCss(link, link.href.split("?")[0] + "?unix=1591770796565");
           });
       }
     }
--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "986e1b5d973dc8a707b8";
+/******/ 	var hotCurrentHash = "681bd0c6624698ff1445";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
